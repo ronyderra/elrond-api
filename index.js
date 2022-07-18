c15279553dff849be808c26e4b04e775c3cdd291
ronyXnes <derrarony@gmail.com>
2022-07-18 21:16:30 +0300

    elrond

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const axios = require("axios");
 const reader = require("xlsx");
-const file = reader.readFile("./18_7_goodTransactions.xlsx");
 const { Base64 } = require("js-base64");
-const fs = require('fs');
-const TelegramBot = require('node-telegram-bot-api');
-
-const token = '5285257815:AAE7Rzxj2U68im8HKGbjfgRkdmTMwyoPO4o';
+const fs = require("fs");
+const TelegramBot = require("node-telegram-bot-api");
+var cron = require("node-cron");
+const token = "5285257815:AAE7Rzxj2U68im8HKGbjfgRkdmTMwyoPO4o";
 const GROUP_ID = "-778913594";
-const FILE = "./18_7_goodTransactions.xlsx";
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 const api1 = axios.create({
   baseURL: "https://api.elrond.com",
@@ -18,36 +16,23 @@ const api1 = axios.create({
   },
 });
 
-const getData = async () => {
-  console.log("here")
+const getData = async (fileName) => {
   try {
+
+console.log("got here")
+    const file = reader.readFile("./" + fileName);
+
     let res = await api1.get(
-      `/accounts/erd1qqqqqqqqqqqqqpgq3y98dyjdp72lwzvd35yt4f9ua2a3n70v0drsfycvu8/transfers?from=0&size=10000`
+      `/accounts/erd1qqqqqqqqqqqqqpgq3y98dyjdp72lwzvd35yt4f9ua2a3n70v0drsfycvu8/transfers?from=50&size=100`
     );
     console.log(res.data.length);
-    await smartContractResults1(res.data);
+    await smartContractResults1(res.data, file, fileName);
   } catch (err) {
     console.log(err);
   }
 };
 
-const transactionCall = async (hash) => {
-  let i = 0;
-  while (i < 200) {
-    try {
-      let res = await axios.get(`https://api.elrond.com/transaction/${hash}?withResults=true`);
-      if (res.data) {
-        return res;
-      }
-      i++;
-    } catch {
-      i++;
-      continue;
-    }
-  }
-};
-
-const smartContractResults1 = async (dataArray) => {
+const smartContractResults1 = async (dataArray, aFile, fileName) => {
   console.log(dataArray.length);
   try {
     let data = [];
@@ -82,24 +67,24 @@ const smartContractResults1 = async (dataArray) => {
 
       const collection = logs.events[0].topics[0];
       const collectionName = Base64.decode(collection);
-      console.log(collectionName)
+      console.log(collectionName);
 
-      console.log("####hashToCall####:" , hashToCall);
+      console.log("####hashToCall####:", hashToCall);
 
       const transactionFailed = smartContractResults.find(
         (el) =>
           Object.keys(el).indexOf("returnMessage") === 11 &&
           el.returnMessage === "sending value to non payable contract"
       );
-      if (transactionFailed){
-        console.log("transaction Failed" , hashToCall)
+      if (transactionFailed) {
+        console.log("transaction Failed", hashToCall);
         continue;
-      } 
+      }
 
       for (let i = 0; i < smartContractResults.length; i++) {
         if (
           smartContractResults[i].receiver ===
-         "erd1qqqqqqqqqqqqqpgq3y98dyjdp72lwzvd35yt4f9ua2a3n70v0drsfycvu8" &&
+            "erd1qqqqqqqqqqqqqpgq3y98dyjdp72lwzvd35yt4f9ua2a3n70v0drsfycvu8" &&
           smartContractResults[i].returnMessage !== "sending value to non payable contract"
         ) {
           const internalHashValue = smartContractResults[i].value;
@@ -109,7 +94,6 @@ const smartContractResults1 = async (dataArray) => {
           const isAroyaltyHash = internalHashValue == tenPercentOfMainValue ? true : false;
 
           if (isAroyaltyHash) {
-
             const newobj = {
               royaltyValue: internalHashValue / 1000000000000000000,
               mainValue: mainHashValue / 1000000000000000000,
@@ -125,25 +109,66 @@ const smartContractResults1 = async (dataArray) => {
       }
     }
     console.log("data____________", data);
-    console.log("royalties:" , royalties);
+    console.log("royalties:", royalties);
+
     const ws = reader.utils.json_to_sheet(data);
-    reader.utils.book_append_sheet(file, ws, "Sheet101");
-    reader.writeFile(file, "./18_7_goodTransactions.xlsx");
-    await bot.sendDocument(
-      GROUP_ID,
-      fs.readFileSync(FILE),
-      {
-          caption: 'Daily Report'
-      },
-      {
-          filename: '18_7_goodTransactions.xlsx',
-          contentType: 'application/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-      })
+    reader.utils.book_append_sheet(aFile, ws);
+    reader.writeFile(aFile, "./" + fileName);
+
+    await bot
+      .sendDocument(
+        GROUP_ID,
+        fs.readFileSync("./" + fileName),
+        {
+          caption: "total royalyies:" + royalties + " EGLD",
+        },
+        {
+          filename: fileName,
+          contentType:
+            "application/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        }
+      )
       .then(() => {
-          console.log('File has been sent');
+        console.log("File has been sent");
       });
+
   } catch (err) {
     console.log(err.message);
   }
 };
-getData();
+
+cron.schedule("* * * * *",async () => {
+  console.log("running a task every minute");
+  const fileName =Date(Date.now())
+ await openFile(fileName + ".xlsx")
+
+
+ setTimeout( () =>{
+   getData(fileName.toString() + ".xlsx");
+}, 5000);
+ 
+});
+
+const openFile = (fileName) =>{
+  fs.open(fileName.toString(), "w", function (err, file) {
+    if (err) throw err;
+    console.log("Saved!");
+  });
+}
+
+
+const transactionCall = async (hash) => {
+  let i = 0;
+  while (i < 200) {
+    try {
+      let res = await axios.get(`https://api.elrond.com/transaction/${hash}?withResults=true`);
+      if (res.data) {
+        return res;
+      }
+      i++;
+    } catch {
+      i++;
+      continue;
+    }
+  }
+};
